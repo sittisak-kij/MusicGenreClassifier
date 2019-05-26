bbbd285570b1f296e629f873ad26e920176aa4de
Sittisak <sittisak.kij@gmail.com>
2019-05-27 00:29:27 +0700

    chage port

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 UPLOAD_FOLDER = os.getcwd() + '\\uploads'
 
 app = Flask(__name__)
+app.run(host= '0.0.0.0', port="80")
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
